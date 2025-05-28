fe8b6c368b6695325a2d6c4d35dba10efd083b49
MangoRRe <snowrsk@gmail.com>
2025-05-28 16:01:24 +0900

    first commit

---
+++ src/main/java/com/framework/register/mapper/RegisterMapper.java
@@ -0,0 +1,15 @@
+package com.framework.register.mapper;
+
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RegisterMapper {
+	
+	int selectMemberDuplicateCount(Map<String, Object> params);
+	
+	int insertMember(Map<String, Object> params);
+	
+	Map<String, Object> selectMemberInfo(Map<String, Object> params);
+}
